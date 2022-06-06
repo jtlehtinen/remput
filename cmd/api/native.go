e145200217e7085a5b3f3f92ce888f58eaec8416
Juha Lehtinen <jtlehtinen@fastmail.com>
2022-06-06 14:54:09 +0300

    refactor: clean up native code

--- cmd/api/native.go
+++ cmd/api/native.go
@@ -5,12 +5,14 @@ import (
 )
 
 var (
-	native                    = syscall.NewLazyDLL("native.dll")
-	nativeSendKeyDown         = native.NewProc("send_key_down")
-	nativeSendKeyUp           = native.NewProc("send_key_up")
-	nativeSendMouseButtonDown = native.NewProc("send_mouse_button_down")
-	nativeSendMouseButtonUp   = native.NewProc("send_mouse_button_up")
-	nativeSendMouseMove       = native.NewProc("send_mouse_move")
+	native                     = syscall.NewLazyDLL("native.dll")
+	nativeSendKeyDown          = native.NewProc("send_key_down")
+	nativeSendKeyUp            = native.NewProc("send_key_up")
+	nativeSendKey              = native.NewProc("send_key")
+	nativeSendMouseButtonDown  = native.NewProc("send_mouse_button_down")
+	nativeSendMouseButtonUp    = native.NewProc("send_mouse_button_up")
+	nativeSendMouseButtonClick = native.NewProc("send_mouse_button_click")
+	nativeSendMouseMove        = native.NewProc("send_mouse_move")
 )
 
 func sendKeyDown(key int) {
@@ -21,12 +23,22 @@ func sendKeyUp(key int) {
 	_, _, _ = syscall.SyscallN(nativeSendKeyUp.Addr(), uintptr(key))
 }
 
+// sendKey sends a key down event followed by a key up event.
+func sendKey(key int) {
+	_, _, _ = syscall.SyscallN(nativeSendKey.Addr(), uintptr(key))
+}
+
 func sendMouseButtonDown(button int) {
 	_, _, _ = syscall.SyscallN(nativeSendMouseButtonDown.Addr(), uintptr(button))
 }
 
 func sendMouseButtonUp(button int) {
 	_, _, _ = syscall.SyscallN(nativeSendMouseButtonUp.Addr(), uintptr(button))
+}
+
+// sendMouseButtonClick sends a button down event followed by a button up event.
+func sendMouseButtonClick(button int) {
+	_, _, _ = syscall.SyscallN(nativeSendMouseButtonClick.Addr(), uintptr(button))
 }
 
 func sendMouseMove(dx, dy int) {
--- native/win32.c
+++ native/win32.c
@@ -4,85 +4,58 @@
 #define NOMINMAX
 #include <Windows.h>
 
-__declspec(dllexport) void send_key_down(int key) {
-  INPUT input = {
-    .type = INPUT_KEYBOARD,
-    .ki = {
-      .wVk = key,
-    },
-  };
+static void send_key_event(int key, DWORD flags) {
+  // @TODO: Validate key...
+  INPUT input = {0};
+  input.type = INPUT_KEYBOARD;
+  input.ki.wVk = key;
+  input.ki.dwFlags = flags;
+  SendInput(1, &input, sizeof(INPUT));
+}
 
-  if (SendInput(1, &input, sizeof(INPUT)) == 0) {
-    DWORD err = GetLastError();
-    // @TODO: handle error
-  }
+__declspec(dllexport) void send_key_down(int key) {
+  send_key_event(key, 0);
 }
 
 __declspec(dllexport) void send_key_up(int key) {
-  INPUT input = {
-    .type = INPUT_KEYBOARD,
-    .ki = {
-      .wVk = key,
-      .dwFlags = KEYEVENTF_KEYUP,
-    },
-  };
+  send_key_event(key, KEYEVENTF_KEYUP);
+}
 
-  if (SendInput(1, &input, sizeof(INPUT)) == 0) {
-    DWORD err = GetLastError();
-    // @TODO: handle error
-  }
+__declspec(dllexport) void send_key(int key) {
+  send_key_down(key);
+  send_key_up(key);
+}
+
+static void send_mouse_event(int dx, int dy, DWORD flags) {
+  INPUT input = {0};
+  input.type = INPUT_MOUSE;
+  input.mi.dx = dx;
+  input.mi.dy = dy;
+  input.mi.dwFlags = flags;
+  SendInput(1, &input, sizeof(INPUT));
 }
 
 __declspec(dllexport) void send_mouse_button_down(int button) {
-  DWORD flags = 0;
-  if (button == VK_LBUTTON) flags = MOUSEEVENTF_LEFTDOWN;
-  if (button == VK_RBUTTON) flags = MOUSEEVENTF_RIGHTDOWN;
-  if (button == VK_MBUTTON) flags = MOUSEEVENTF_MIDDLEDOWN;
-
-  INPUT input = {
-    .type = INPUT_MOUSE,
-    .mi = {
-      .dwFlags = flags,
-    },
-  };
-
-  if (SendInput(1, &input, sizeof(INPUT)) == 0) {
-    DWORD err = GetLastError();
-    // @TODO: handle error
+  switch (button) {
+    case VK_LBUTTON: send_mouse_event(0, 0, MOUSEEVENTF_LEFTDOWN); break;
+    case VK_RBUTTON: send_mouse_event(0, 0, MOUSEEVENTF_RIGHTDOWN); break;
+    case VK_MBUTTON: send_mouse_event(0, 0, MOUSEEVENTF_MIDDLEDOWN); break;
   }
 }
 
 __declspec(dllexport) void send_mouse_button_up(int button) {
-  DWORD flags = 0;
-  if (button == VK_LBUTTON) flags = MOUSEEVENTF_LEFTUP;
-  if (button == VK_RBUTTON) flags = MOUSEEVENTF_RIGHTUP;
-  if (button == VK_MBUTTON) flags = MOUSEEVENTF_MIDDLEUP;
-
-  INPUT input = {
-    .type = INPUT_MOUSE,
-    .mi = {
-      .dwFlags = flags,
-    },
-  };
-
-  if (SendInput(1, &input, sizeof(INPUT)) == 0) {
-    DWORD err = GetLastError();
-    // @TODO: handle error
+  switch (button) {
+    case VK_LBUTTON: send_mouse_event(0, 0, MOUSEEVENTF_LEFTUP); break;
+    case VK_RBUTTON: send_mouse_event(0, 0, MOUSEEVENTF_RIGHTUP); break;
+    case VK_MBUTTON: send_mouse_event(0, 0, MOUSEEVENTF_MIDDLEUP); break;
   }
 }
 
-__declspec(dllexport) void send_mouse_move(int dx, int dy) {
-  INPUT input = {
-    .type = INPUT_MOUSE,
-    .mi = {
-      .dx = dx,
-      .dy = dy,
-      .dwFlags = MOUSEEVENTF_MOVE,
-    },
-  };
+__declspec(dllexport) void send_mouse_button_click(int button) {
+  send_mouse_button_down(button);
+  send_mouse_button_up(button);
+}
 
-  if (SendInput(1, &input, sizeof(INPUT)) == 0) {
-    DWORD err = GetLastError();
-    // @TODO: handle error
-  }
+__declspec(dllexport) void send_mouse_move(int dx, int dy) {
+  send_mouse_event(dx, dy, MOUSEEVENTF_MOVE);
 }
